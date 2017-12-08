2b622bb385c5e2ad40c496e903dc9eca9c5e0e09
Mohit Gawande <mohitgawande@Mohits-MacBook-Air.local>
2017-12-08 18:33:26 +0530

    UI enhancements & notification for buy and sell alarm

--- popup.js
+++ popup.js
@@ -5,6 +5,37 @@
     var quantity_type_multiplier = null;
     var buy_price = null;
 
+    var showPriceNotification = function(title, message, items){
+      chrome.notifications.create(new Date().getTime()+ 'notification', {
+        type : 'list',
+        title : title,
+        message : message,
+        items : items,
+        iconUrl : './icon.png',
+        // imageUrl : './icon.png',
+        isClickable : true
+      }, function(){
+        // console.log('notificationsnotifications data', arguments)
+      })
+    }
+    
+    var handleNotification = function(data){
+      var buyAlarm = parseFloat(document.getElementsByName('buy_alarm')[0].value);
+      var sellAlarm = parseFloat(document.getElementsByName('sell_alarm')[0].value);
+      // console.log('datatatata buyyy', data.buy, buyAlarm + 5000, data.buy < buyAlarm + 5000)
+      // console.log('datatatata selll', data.sell, sellAlarm + 5000, data.sell > sellAlarm - 5000)
+      
+      if((buyAlarm && data.buy < buyAlarm + 5000) || (sellAlarm && data.sell > sellAlarm - 5000)){
+        
+        console.log((buyAlarm && data.buy < buyAlarm + 5000), (sellAlarm && data.sell > sellAlarm - 5000))
+        var title = data.buy < buyAlarm + 5000 ? 'BUY PRICE IS CLOSE' : 'SELL PRICE IS CLOSE';
+        var message = 'HERE IS MY MESSAGE FOR YOU' ;
+        var items = [
+            { title: "BUY: " + data.buy, message: "SELL: " + data.sell}
+        ]
+        showPriceNotification(title, message, items)
+      }
+    }
     var getZebPayData = function(){
       return new Promise(function(success, failure){
         fetch('https://api.zebpay.com/api/v1/ticker?currencyCode=INR')
@@ -14,12 +45,12 @@
             throw error
           })
           .then(function(data){
-              // console.log(data)
+              handleNotification(data)
               document.getElementsByName('zebpay_buy_price')[0].value = (data.buy * quantity_type_multiplier).toFixed(2);
               document.getElementsByName('zebpay_sell_price')[0].value = (data.sell * quantity_type_multiplier).toFixed(2);
               document.getElementsByName('zebpay_net_buy_price')[0].value = (data.buy * quantity_hold * quantity_type_multiplier).toFixed(2);
               document.getElementsByName('zebpay_net_sell_price')[0].value = (data.sell * quantity_hold * quantity_type_multiplier).toFixed(2);
-              console.log('hiiiiiii 3###########', parseInt(document.getElementsByName('zebpay_net_sell_price')[0].value), buy_price, parseInt(buy_price))
+              document.getElementById('bitcoin-price-zebpay').innerText = document.getElementsByName('zebpay_buy_price')[0].value + '  /  ' + document.getElementsByName('zebpay_sell_price')[0].value;
               if(buy_price)
               if(parseInt(document.getElementsByName('zebpay_net_sell_price')[0].value) < parseInt(buy_price)){
                 var zebpayItems = document.getElementsByClassName('zebpay-item')
@@ -59,8 +90,6 @@
             document.getElementsByName('coindesk_sell_price')[0].value = (data.sell * quantity_type_multiplier).toFixed(2);
             document.getElementsByName('coindesk_net_buy_price')[0].value = (data.buy * quantity_hold * quantity_type_multiplier).toFixed(2);
             document.getElementsByName('coindesk_net_sell_price')[0].value = (data.sell * quantity_hold * quantity_type_multiplier).toFixed(2);
-            
-            console.log('hiiiiiii 3###########', parseInt(document.getElementsByName('coindesk_net_sell_price')[0].value), buy_price, parseInt(buy_price))
             if(buy_price)
             if(parseInt(document.getElementsByName('coindesk_net_sell_price')[0].value) < parseInt(buy_price)){
               var coindeskItems = document.getElementsByClassName('coindesk-item')
@@ -80,17 +109,19 @@
       })
     }
     var getData = function(){
-      console.log('hiiiiiiiii')
       document.getElementById('loading_indicator').style.display = ''
       quantity_hold = document.getElementsByName('quantity_hold')[0].value
       quantity_type_multiplier = document.getElementsByName('quantity_type')[0].value == 'btc' ? 1 : (1/1000000);
       buy_price = document.getElementsByName('buy_price')[0].value
+      // console.log('hiiiiiiiii', quantity_hold, quantity_type_multiplier, buy_price)
       if(chrome.storage){
         chrome.storage.sync.set({
           quantity_type_multiplier : quantity_type_multiplier, 
           quantity_hold : quantity_hold, 
           quantity_type : document.getElementsByName('quantity_type')[0].value,
-          buy_price : document.getElementsByName('buy_price')[0].value
+          buy_price : document.getElementsByName('buy_price')[0].value,
+          buy_alarm : parseFloat(document.getElementsByName('buy_alarm')[0].value),
+          sell_alarm : parseFloat(document.getElementsByName('sell_alarm')[0].value),
         });
       }
       Promise.all([
@@ -99,7 +130,7 @@
       ]).then(function(data){
         document.getElementById('loading_indicator').style.display = 'none'
         var source = ['zebpay', 'coindesk'];
-        console.log('got data')
+        // console.log('got data')
       })
     }
     if(chrome.storage){
@@ -108,6 +139,10 @@
         document.getElementsByName('quantity_type')[0].value = items.quantity_type ? items.quantity_type : 'btc'
         quantity_type_multiplier = items.quantity_type == 'btc' ? 1 : (items.quantity_type == 'bits' ? (1/1000000) : 0)
         document.getElementsByName('buy_price')[0].value = buy_price = items.buy_price ? items.buy_price : null
+
+        document.getElementsByName('buy_alarm')[0].value = items.buy_alarm
+        document.getElementsByName('sell_alarm')[0].value = items.buy_alarm
+
         setTimeout(getData, 0)
         setInterval(getData, 4000)
         document.getElementById('fetchDataButton').onclick = getData
@@ -116,8 +151,8 @@
     else{
       quantity_hold = document.getElementsByName('quantity_hold')[0].value;
       quantity_type_multiplier = document.getElementsByName('quantity_type')[0].value == 'btc' ? 1 : (document.getElementsByName('quantity_type')[0].value == 'bits' ? (1/1000000) : 0)
-      console.log("document.getElementsByName('quantity_type')[0].value", document.getElementsByName('quantity_type')[0].value)
-      console.log('quantity_type_multiplierquantity_type_multiplier', quantity_type_multiplier)
+      // console.log("document.getElementsByName('quantity_type')[0].value", document.getElementsByName('quantity_type')[0].value)
+      // console.log('quantity_type_multiplierquantity_type_multiplier', quantity_type_multiplier)
       setTimeout(getData, 0)
       setInterval(getData, 4000)
       document.getElementById('fetchDataButton').onclick = getData
